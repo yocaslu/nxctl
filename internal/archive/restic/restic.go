ae1ee8605c1175afa5af28893a28fbba2190d9ce
Lucas <lsantos2003silva@gmail.com>
2026-09-11 17:29:09 +0000

    create restic repository

--- internal/archive/restic/restic.go
+++ internal/archive/restic/restic.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"nxctl/internal/proc"
+	"nxctl/internal/utils"
 	"os"
 )
 
@@ -13,24 +14,24 @@ type Restic struct {
 	_env     []string
 }
 
-func New(repo string, password string) (*Restic, error) {
+func New(repo string, password string) *Restic {
 	// TODO: check if repo exists
 
 	return &Restic{
 		Repo:     repo,
 		Password: password,
 		_env:     os.Environ(),
-	}, nil
+	}
 }
 
 func Load() (*Restic, error) {
 	repo := os.Getenv("RESTIC_REPOSITORY")
-	if repo != "" {
+	if repo == "" {
 		return nil, fmt.Errorf("RESTIC_REPOSITORY is missing!")
 	}
 
 	password := os.Getenv("RESTIC_PASSWORD")
-	if password != "" {
+	if password == "" {
 		return nil, fmt.Errorf("RESTIC_PASSWORD is missing!")
 	}
 
@@ -41,10 +42,35 @@ func Load() (*Restic, error) {
 	}, nil
 }
 
+func (r *Restic) CreateRepo() error {
+	exist, _ := utils.DirExist(r.Repo)
+	if exist {
+		return nil
+	}
+
+	log.Printf("creating restic repository in %s\n", r.Repo)
+	stdout, err := proc.Run(os.Environ(), "restic", "-r", r.Repo, "init")
+	if err != nil {
+		log.Printf("failed to create restic repository in %s:\n%s\n", r.Repo, err)
+		return err
+	}
+
+	log.Println(stdout)
+	return nil
+}
+
 func (r *Restic) CreateSnapshot(target string) error {
+	exist, _ := utils.DirExist(r.Repo)
+	if !exist {
+		err := r.CreateRepo()
+		if err != nil {
+			return err
+		}
+	}
+
 	stdout, err := proc.Run(r._env, "restic", "-r", r.Repo, "backup", target)
 	if err != nil {
-		log.Printf("failed to backup %s", target)
+		log.Printf("failed to snapshot %s:\n%s\n", target, err)
 		return err
 	}
 
